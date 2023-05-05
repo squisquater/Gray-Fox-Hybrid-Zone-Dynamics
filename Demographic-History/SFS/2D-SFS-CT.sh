e28b1d91fb0d37da486e901b311f8b7f59d74b5e
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2023-05-04 17:46:55 -0700

    Create 2D-SFS-CT.sh

---
+++ Demographic-History/SFS/2D-SFS-CT.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH --job-name=2D-SFS-CT
+#SBATCH --nodes 1
+#SBATCH --time 12:00:00
+#SBATCH --mem 5GB
+#SBATCH -p bmm
+#SBATCH -A ctbrowngrp
+#SBATCH -o /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-CT.out
+#SBATCH -e /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-CT.err
+
+module load angsd
+
+echo "Generating 2-D Site Frequency Spectrum for all listed populations:"
+
+cd /home/sophiepq/GrayFox/angsd/SFS
+
+POP0=CaliforniaFullK3HC #n=26 > 52 gene copies
+POP1=TexasFullK3HC #n=87 > 174 gene copies
+
+realSFS ${POP0}.saf.idx ${POP1}.saf.idx > 2D-SFS-ET.sfs
