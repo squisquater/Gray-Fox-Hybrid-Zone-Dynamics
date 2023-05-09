9fdb6b177504f9238380179ce18f77503cc1e70d
Sophie Preckler-Quisquater <squisquater@ucdavis.edu>
2023-05-09 13:34:09 -0700

    Update and rename 2D-SFS-ET.sh to 2D-SFS-ET-FullK3HC.sh

--- Demographic-History/SFS/2D-SFS-ET-FullK3HC.sh
+++ Demographic-History/SFS/2D-SFS-ET-FullK3HC.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -l
-#SBATCH --job-name=2D-SFS-ET
+#SBATCH --job-name=2D-SFS-ET-FullK3HC
 #SBATCH --nodes 1
 #SBATCH --time 12:00:00
 #SBATCH --mem 5GB
 #SBATCH -p bmm
 #SBATCH -A ctbrowngrp
-#SBATCH -o /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-ET.out
-#SBATCH -e /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-ET.err
+#SBATCH -o /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-ET-FullK3HC.out
+#SBATCH -e /home/sophiepq/GrayFox/angsd/SFS/2D-SFS-ET-FullK3HC.err
 
 module load angsd
 
@@ -17,4 +17,4 @@ cd /home/sophiepq/GrayFox/angsd/SFS
 POP0=EastFullK3HC #n=69 > 138 gene copies
 POP1=TexasFullK3HC #n=87 > 174 gene copies
 
-realSFS ${POP0}.saf.idx ${POP1}.saf.idx > 2D-SFS-ET.sfs
+realSFS ${POP0}.saf.idx ${POP1}.saf.idx > 2D-SFS-ET-FullK3HC.sfs
